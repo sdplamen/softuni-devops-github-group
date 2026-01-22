0a951d6421e3653b573be6e462642fea9ab4c8be
Plamen Stoilkov <109562327+sdplamen@users.noreply.github.com>
2026-01-22 09:24:54 +0200

    Delete Resources

--- Resources/3-BooksApi/Books.Infrastructure/Connection.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Books.Infrastructure
-{
-    public class Connection
-    {
-        public const string ConnectionString = @"Server=.;Database=Books;Integrated Security=True;Encrypt=False";
-    }
-}
